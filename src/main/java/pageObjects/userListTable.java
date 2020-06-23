9c3c6ce077c03f9e6ba2da11af7bcc14c1f4999f
tchicheko <tchicheko@gmail.com>
2020-06-23 23:57:38 +0200

    Task 2 second commit

--- src/main/java/pageObjects/userListTable.java
+++ src/main/java/pageObjects/userListTable.java
@@ -7,82 +7,106 @@ import org.openqa.selenium.support.ui.Select;
 
 public class userListTable {
 
-	
+	//Declaring variables
 	public WebDriver driver;
+	By adduser = By.xpath("/html/body/table/thead/tr[2]/td/button");
+	By name = By.xpath("//input[contains(@name,'FirstNa')]");
+	By surname = By.xpath("//input[contains(@name,'LastNa')]");
+	By username = By.xpath("//input[contains(@name,'UserNa')]");
+	By password = By.xpath("//input[contains(@name,'Passwo')]");
+	By email = By.xpath("//input[contains(@name,'Emai')]");
+	By cellphone = By.xpath("//input[contains(@name,'Mobilepho')]");
+	By submit = By.xpath("/html/body/div[3]/div[3]/button[2]");
+	By companyAAA = By.xpath("//input[@value='15']");
+	By companyBBB = By.xpath("//input[@value='16']");
+	By tableHeader = By.xpath("/html/body/table/thead/tr[3]");
+	By table = By.xpath("/html/body/table");
 	
+	
+	//Constructor for the class object
 	public userListTable (WebDriver driver) {
 		
 		this.driver=driver;
 	}
 	
-	By adduser = By.xpath("/html/body/table/thead/tr[2]/td/button");
+	//Getting Table  Element 		
+			public WebElement getTable() {
+				
+					return driver.findElement(table);
+			}
+	
+	//Getting Table Header Element 		
+		public WebElement getTableHeader() {
 			
+				return driver.findElement(tableHeader);
+		}
+		
+	
+	//Getting Add User button Element 		
 	public WebElement getAddUserButton() {
 		
 			return driver.findElement(adduser);
 	}
 	
-	By name = By.xpath("//input[contains(@name,'FirstNa')]");
+	//Getting Name input field Element 
 	public WebElement getName() {
 		
 		return driver.findElement(name);
 }
 	
-	By surname = By.xpath("//input[contains(@name,'LastNa')]");
+	//Getting Surname input field Element 
 	public WebElement getSurname() {
 		
 		return driver.findElement(surname);
 }
 	
-	By username = By.xpath("//input[contains(@name,'UserNa')]");
+	//Getting Username input field Element 
 	public WebElement getUsername() {
 		
 		return driver.findElement(username);
 }
 	
-	By password = By.xpath("//input[contains(@name,'Passwo')]");
+	//Getting Password input field Element 
 	public WebElement getPassword() {
 		
 		return driver.findElement(password);
 }
 	
-	By email = By.xpath("//input[contains(@name,'Emai')]");
+	//Getting Email input field Element 
 	public WebElement getEmail() {
 		
 		return driver.findElement(email);
 }
 	
-	By cellphone = By.xpath("//input[contains(@name,'Mobilepho')]");
+	//Getting Cell Phone Number input field Element
 	public WebElement getCellPhone() {
 		
 		return driver.findElement(cellphone);
 }
 	
-	By submit = By.xpath("/html/body/div[3]/div[3]/button[2]");
+	//Getting Submit button field Element
 	public WebElement getSubmitbutton() {
 		
 		return driver.findElement(submit);
 }
 	
-	
+	//Creating an object for the drop down field Element
 	public Select getRole() {
 		
 		Select role= new Select (driver.findElement(By.xpath("//select[contains(@name,'RoleI')]")));
 		return role;
 }
 	
-	By companyAAA = By.xpath("//input[@value='15']");
+	//Getting Company AAA option field element
 	public WebElement getCompanyAAA() {
 		
 		return driver.findElement(companyAAA);
 }
 	
-	By companyBBB = By.xpath("//input[@value='16']");
+	//Getting Company BBB option field element
 	public WebElement getCompanyBBB() {
 		
 		return driver.findElement(companyBBB);
 }
 	
-	//tagName[contains(@attribute,'value')]   /html/body/table/thead/tr[2]/td/button
-	//button[contains(@type,'ad')]  /html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input
 }
--- src/test/java/Assessment/AddUser.java
+++ src/test/java/Assessment/AddUser.java
@@ -1,50 +1,71 @@
 package Assessment;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
 
 import Resources.base;
 import pageObjects.userListTable;
 
 public class AddUser extends base {
 	
+	public static Logger log =LogManager.getLogger(base.class.getName());
 	excelUtility d= new excelUtility();
 	
-	@Test(dataProvider="getData")
-	public void addUser(String name, String surname, String username, String password,String customer,String role, String email,String cellphone ) throws IOException {
-	
-		driver= initializeDriver();
-		driver.get(prop.getProperty("url"));
+	@BeforeTest
+	public void inititialize() throws IOException {
 		
+		//Webdriver initialization
+		driver= initializeDriver();
+		log.info("Driver is initialised");
+		
+		driver.get(prop.getProperty("url"));
+		log.info("Navigated to Userlist webpage ");
+	}
+	
+	
+	@Test(dataProvider="getData")
+	public void addUser(String name, String surname, String username, String password,String customer,String role, String email,String cellphone ) {
+		
+		
+		//Creating object for pageObject model  userListTable class
 		userListTable userlist= new userListTable(driver);
+		log.info("userlist object created");
 		
 		//Validating that user is on User List table
-		Assert.assertTrue(userlist.getAddUserButton().isDisplayed());
+		Assert.assertTrue(userlist.getTableHeader().isDisplayed());
+		log.info("Validation done for User List Table done");
 		
-		
+		//Parsing data and performing actions to web elements
 		userlist.getAddUserButton().click();
-		
-		
 		userlist.getName().sendKeys(name);
-		userlist.getSurname().sendKeys(surname);
+		userlist.getSurname().sendKeys(surname);		
 		userlist.getUsername().sendKeys(username);
 		userlist.getPassword().sendKeys(password);
 		
+		//Checking option to select for the Customer field considering data provided by the DataProvider
 		if(customer.equals("Company AAA")) {
 			
 			userlist.getCompanyAAA().click();
+			log.info("Customer is Company AAA");
+			
 		}
 		else {
 			
 			userlist.getCompanyBBB().click();
+			log.info("Customer is Company BBB");
 			
 		}
 		
-		
+		//Checking option to select for the Role field considering data provided by the DataProvider
 		if(role.equals("Sales Team")) {
 			
 			userlist.getRole().selectByValue("0");
@@ -61,17 +82,34 @@ public class AddUser extends base {
 				userlist.getRole().selectByValue("2");
 			}
 		
+		//Parsing data and performing actions to web elements
 		userlist.getEmail().sendKeys(email);
 		userlist.getCellPhone().sendKeys(cellphone);
 		
 		
 		
-		//Submit to add user
+		//Click submit to add user
 		userlist.getSubmitbutton().click();
-		driver.close();
+		log.info("Submit button clicked");
+		
+		//Ensuring user has been added to the list
+		Assert.assertEquals(userlist.getTable(), username);
+		
 		
 		
 	}
+	
+	//Closing the browser
+	@AfterTest
+	public void teardown() {
+		
+		driver.close();
+		log.info("Browser closed");
+		
+	}
+	
+	
+	
 	
 	
 	@DataProvider
@@ -97,6 +135,7 @@ public class AddUser extends base {
 		users[1][7]="82555";
 		
 		return users;
+		
 	}
 	
 
